7f79e71274436eafa68997feb8e9c3c4050e2a00
Mr Ankit <ankitsabariya@gmail.com>
2025-08-27 20:16:43 +0530

    State

---
+++ 04-Usestate(counter)-Hooks/src/App.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+
+function App() {
+  let [count, setCount] = useState(156);
+  function handleInc() {
+    count = count + 1;
+    setCount(count);
+    console.log(count); // Print To hoga ui Render nhi isi liye state ka use hoga
+  }
+  let handleDec = () => {
+    count = count - 1;
+    setCount(count);
+    console.log(count);
+  };
+  return (
+    <>
+      <div>Counter : {count}</div>
+      <button onClick={handleInc}>+</button>&nbsp;
+      <button onClick={handleDec}>-</button>
+    </>
+  );
+}
+
+export default App;
